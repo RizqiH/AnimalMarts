5b1cec2694f0221c24c830a345e1029553012a5c
RizqiH <rizkiamanan@gmail.com>
2025-06-02 01:29:32 +0700

    done

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -17,8 +17,7 @@ import {
 import { useCart } from "../context/CartContext";
 
 // API Base URL - sesuaikan dengan backend Anda
-const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || "https://backend-animalmart.vercel.app";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
 
 const AnimalMartCheckout = () => {
   const {
